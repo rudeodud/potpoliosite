010bf667804f242de7b7d4f45b763dbb205f76d0
Gyeongdaeyoung <86515840+rudeodud@users.noreply.github.com>
2025-09-10 18:24:06 +0900

    update

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.set("port", process.env.PORT || 5001);
+
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(app.get("port"), () => {
+  console.log(app.get("port"), "번에서 시작 중");
+});
